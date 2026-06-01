5fee810b214c3dcf4a058b16cc01f27ec2e31a0c
2024131095 <yeheepark@kau.kr>
2026-06-01 16:10:11 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,15 +2,17 @@
 
 int main(){
     char arr[11]; // 10개의 문자를 저장할 배열
+    char *p = nullptr;
+    char *q = nullptr;
     scanf("%s", arr); //10개의 문자 입력
 
     char max_char = arr[0]; //가장 많이 나타난 문자를 저장할 변수
     int max_count = 0; //그 빈도수를 저장할 변수
 
-    for (char *p = arr; p < arr + 10; p++) { //외부 반복분용 포인터 (배열의 처음부터 끝까지 이동)
+    for (p = arr; p < arr + 10; p++) { //외부 반복분용 포인터 (배열의 처음부터 끝까지 이동)
         int current_count = 0;
 
-    for (char*q=arr; q<arr+10; q++){ // 내부 반복문용 포인터 (q가 가리키는 문자가 배열에 몇 개 있는지 찾음)
+    for (q=arr; q<arr+10; q++){ // 내부 반복문용 포인터 (q가 가리키는 문자가 배열에 몇 개 있는지 찾음)
         if(*p==*q){
             current_count++;
         }
