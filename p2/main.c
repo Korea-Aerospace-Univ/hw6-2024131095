757c2d5f710724c285e26a6931ee598cc17e5638
2024131095 <yeheepark@kau.kr>
2026-06-01 15:55:03 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -4,22 +4,25 @@ int main(){
     int N;
     int arr1[20]; // 첫번째 숫자 배열
     int arr2[20]; // 두번째 숫자 배열
+    int *p = nullptr;
+    int *p1 = nullptr;
+    int *p2 = nullptr;
 
     scanf("%d", &N); // 정수N입력
 
-    for (int *p = arr1; p<arr1+N; p++){ // 첫번째 배열에 숫자 차례로 넣기
+    for (p = arr1; p < arr1 + N; p++){ // 첫번째 배열에 숫자 차례로 넣기
         scanf("%d", p);
     }
 
-    for (int *p = arr2; p < arr2+N; p++){ // 두번째 배열에 숫자 차레로 넣기
+    for (p = arr2; p < arr2 + N; p++){ // 두번째 배열에 숫자 차레로 넣기
         scanf("%d", p);
     }
 
-    int *p1=arr1; // 첫번째 배열의 맨처음
-    int *p2=arr2+N-1; // 두번째 배열의 맨끝
+    p1=arr1; // 첫번째 배열의 맨처음
+    p2=arr2+N-1; // 두번째 배열의 맨끝
 
     for (int i=0; i<N; i++){
-        int sum = *p1+*p2; // 첫배열의 맨처음 + 두번째배열의 맨끝
+        int sum = *p1 + *p2; // 첫배열의 맨처음 + 두번째배열의 맨끝
         printf(" %d", sum); // 출력시 공백 먼저 출력
 
         p1++; // 다음 칸으로 이동
